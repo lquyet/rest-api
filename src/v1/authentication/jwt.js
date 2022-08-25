d1798a215a5dc7561e700ff5d306da60090a73fa
Jo3yVX <anhquyethgvx@gmail.com>
2022-08-26 02:54:52 +0700

    log error in creating token

--- src/v1/authentication/jwt.js
+++ src/v1/authentication/jwt.js
@@ -4,6 +4,7 @@ const createToken = async (data) => {
     return new Promise((resolve, reject) => {
         jwt.sign(data, process.env.JWT_SECRET, {expiresIn: process.env.JWT_TOKEN_EXPIRE}, (err, token) => {
             if (err) {
+                console.log(err);
                 reject(new Error('Error creating token'));
             } else {
                 resolve(token);
