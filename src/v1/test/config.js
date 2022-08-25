0bc848eef0bfbf13e8c91f120bad1cb8c1c16306
Jo3yVX <anhquyethgvx@gmail.com>
2022-08-26 02:57:44 +0700

    fix wrong env var name

--- src/v1/test/config.js
+++ src/v1/test/config.js
@@ -2,7 +2,7 @@
 process.env.NODE_ENV = 'test';
 process.env.MONGO_URL = 'mongodb://127.0.0.1:27017/rest-api';
 process.env.JWT_SECRET = 'secret';
-process.env.JWT_EXPIRATION = '1d';
+process.env.JWT_TOKEN_EXPIRE = '1h';
 
 const chai = require('chai');
 const chaiHttp = require('chai-http');
